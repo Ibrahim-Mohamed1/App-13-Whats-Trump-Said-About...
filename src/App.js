097a76f9e4494891359b367b2143c25295c24074
Ibrahim Mohamed <ibrahim.mohamedm@hotmail.com>
2019-04-30 15:48:58 -0600

    whats trump said about...

--- src/App.js
+++ src/App.js
@@ -1,26 +1,88 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { withData } from './DataProvider';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super()
+    this.state={
+      search: ""
+    }
+  }
+
+  componentWillMount(){
+    this.props.getTweets()
+  }
+
+  handleChange = (e) => {
+    e.preventDefault()
+    this.setState({
+      search: e.target.value
+    })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    this.props.getTweets(this.state.search)
+    this.setState({
+      search: ""
+    })
+  }
+
+  render() {
+    const styles={
+      form:{
+        textAlign:"center",
+        zoom: 2.3,
+        paddingTop: 15,
+      },
+      button:{
+        display: "block",
+        margin: "auto",
+        marginTop:"1em",
+        zoom: 1.2,
+        border:"white solid",
+        borderRadius: 5,
+        outline: "none"
+      },
+      title:{
+        textAlign:"center", 
+        width: "8 0%", 
+        display:"block",
+        margin:"auto",
+        backgroundColor: '#000000a6',
+        color: "white"
+      }
+    }
+    const mappedTweets = this.props.tweets && this.props.tweets.map(tweet => {
+        return (
+          <div>
+            <h1 style={{color:'white', margin:"1%", padding: "3.5% 0%", textAlign:"center", border: "solid white"}}>{tweet.value}</h1>
+          </div>
+        )
+    })
+    return (
+      <div>
+        <h1 className="title" style={styles.title}>What's Trump said about...</h1>
+        <form style={styles.form} onSubmit={this.handleSubmit} action="">
+          <input 
+            style={{outline:"none", borderRadius: 2, border: "white solid", textAlign:"center"}}
+            type="text" 
+            name="search" 
+            value={this.state.search}
+            onChange={this.handleChange}
+            autoFocus
+            autoComplete='off'
+            placeholder="Topic of interest"
+            required
+          />
+          <button className='button' style={styles.button}>Search</button>
+        </form>
+        {/* <div style={styles.box}> */}
+          {mappedTweets}
+        {/* </div> */}
+      </div>
+    );
+  }
 }
 
-export default App;
+export default withData(App);
---
+++ src/DataProvider.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import axios from "axios"
+const { Provider, Consumer } = React.createContext()
+
+class DataProvider extends Component {
+    constructor(){
+        super()
+        this.state={
+            tweets: []
+        }
+    }
+
+    getTweets = (search)=> {
+        axios.get(`https://vschool-cors.herokuapp.com?url=https://api.tronalddump.io/search/quote?query=${search}`).then(res => {
+          console.log(res.data)
+          this.setState({
+            tweets: res.data._embedded.quotes
+          })
+        })
+      }
+      
+      render() {
+        console.log(this.state.tweets)
+        return (
+            <Provider value={{
+                getTweets: this.getTweets,
+                ...this.state
+            }}
+            >
+                {this.props.children}
+            </Provider>
+        );
+    }
+}
+
+export default DataProvider;
+
+export function withData(C) {
+    return props => <Consumer>{value => <C {...value}{...props} />}</Consumer>
+}
